ed0b68619d2463dac7b4e9c43c3de4687e7417c7
JunlinWang <12112921@mail.sustech.edu.cn>
2023-07-10 09:50:27 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,24 +1,53 @@
-from socket import *
+import rospy
+from geometry_msgs.msg import Twist
+from twisted.internet import reactor, protocol
 
-IP = '10.42.0.1'
-PORT = 9600
-BUFLEN = 512
-listenSocket = socket(AF_INET, SOCK_STREAM)
- 
-listenSocket.bind((IP, PORT))
- 
-listenSocket.listen(1)
-print('The server has already started')
- 
-dataSocket, address = listenSocket.accept()
-print('Connected to ', address)
- 
-while True:
-    data = dataSocket.recv(BUFLEN)
-    if not data:
-        break
-    info = data.decode()
-    dataSocket.send(f'The server received {info.encode()}')
- 
-dataSocket.close()
-listenSocket.close()
+
+class EchoServer(protocol.Protocol):
+    
+    
+    def __init__(self, publisher, rate):
+        super(EchoServer, self).__init__()
+        self.publisher = publisher
+        self.rate = rate
+    
+    
+    def dataReceived(self, data):
+        command = data.decode('utf-8')
+        if command == 'forward':
+            message = Twist()
+            message.linear.x = 1.5
+            self.publisher.publish(message)
+            rate.sleep()
+            
+        elif command == 'backward':
+            message = Twist()
+            message.linear.x = -1.5
+            self.publisher.publish(message)
+            rate.sleep()
+            
+        elif command == 'left':
+            message = Twist()
+            message.angular.z = 0.5
+            self.publisher.publish(message)
+            rate.sleep()
+            
+        elif command == 'right':
+            message = Twist()
+            message.angular.z = -0.5
+            self.publisher.publish(message)
+            rate.sleep()       
+
+
+class EchoServerFactory(protocol.Factory):
+    def __init__(self, publisher, rate):
+        super(EchoServerFactory, self).__init__()
+        self.protocol = EchoServer(publisher, rate)
+
+
+if __name__ == '__main__':
+    rospy.init_node("server", anonymous=True)
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+    rate = rospy.Rate(5)
+    reactor.listenTCP(8000, EchoServerFactory(pub, rate))
+    reactor.run()
